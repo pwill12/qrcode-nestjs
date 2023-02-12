714a19fde284594a27d2570573607ac879148943
unknown <princesolo919@gmail.com>
2023-02-12 06:57:26 +0100

    Initial Commit

--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -12,6 +12,22 @@ export class MovieService {
 
   async findAll(): Promise<Movie[]> {
     const movies = await this.movieModel.find();
+    function shuffle(array: any) {
+      let currentIndex = array.length,  randomIndex: any;
+    
+      while (currentIndex != 0) {
+    
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+    
+        [array[currentIndex], array[randomIndex]] = [
+          array[randomIndex], array[currentIndex]];
+      }
+    
+      return array;
+    }
+    
+    shuffle(movies);
     return movies;
   }
 
